32fec2f48c59bbf04030513d806a7e90ce09146a
Congjian Wang <congjian.wang@inl.gov>
2022-12-20 09:17:20 -0700

    Add initial source codes for Bayesian Calibration

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# Copyright 2022 Battelle Energy Alliance, LLC
+# ALL RIGHTS RESERVED
+#_________________________________________________________________
+#
+#
+#_________________________________________________________________
+from BayCal.src import LikelihoodModel
---
+++ src/LikelihoodModels/Factory.py
@@ -0,0 +1,47 @@
+# Copyright 2022 Battelle Energy Alliance, LLC
+# ALL RIGHTS RESERVED
+#
+#
+#
+"""
+Created on July 30 2020
+@author: Congjian Wang
+"""
+
+from .NormalModel import NormalModel
+
+"""
+ Interface Dictionary (factory) (private)
+"""
+__base = 'LikelihoodBase'
+__interfaceDict = {}
+__interfaceDict['normal'] = NormalModel
+
+def knownTypes():
+  """
+    Returns a list of strings that define the types of instantiable objects for
+    this base factory.
+    @ In, None
+    @ Out, knownTypes, list, the known types
+  """
+  return __interfaceDict.keys()
+
+def returnInstance(classType):
+  """
+    Attempts to create and return an instance of a particular type of object
+    available to this factory.
+    @ In, classType, string, string should be one of the knownTypes.
+    @ Out, returnInstance, instance, subclass object constructed with no arguments
+  """
+  return returnClass(classType)()
+
+def returnClass(classType):
+  """
+    Attempts to return a particular class type available to this factory.
+    @ In, classType, string, string should be one of the knownTypes.
+    @ Out, returnClass, class, reference to the subclass
+  """
+  try:
+    return __interfaceDict[classType.lower()]
+  except KeyError:
+    raise IOError(__name__ + ': unknown ' + __base + ' type ' + classType)
