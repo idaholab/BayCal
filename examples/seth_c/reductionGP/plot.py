329242e61ef9160582ea94f7b7f54781ca95e7e4
Congjian Wang <congjian.wang@inl.gov>
2023-03-30 12:18:40 -0600

    add results

---
+++ examples/seth_c/reductionGP/plot.py
@@ -0,0 +1,46 @@
+import numpy as np
+import scipy.stats as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+#import corner
+import pandas as pd
+
+### control
+fname = 'dumpExport.csv'
+### thining of the chain
+interval = 40
+### load data
+df = pd.read_csv(fname, index_col='traceID')
+index = df.index
+x = range(index[10000], index[-1], interval)
+df = df.loc[x]
+### Histgram plot
+df.hist(alpha=0.5, bins=50)
+### trace plot
+df.plot(subplots=True)
+### Autocorrelation plot
+"""
+Autocorrelation plots are often used for checking randomness in time series.
+This is done by computing autocorrelations for data values at varying time lags.
+If time series is random, such autocorrelations should be near zero for any and
+all time-lag separations. If time series is non-random then one or more of the
+autocorrelations will be significantly non-zero. The horizontal lines displayed
+in the plot correspond to 95% and 99% confidence bands. The dashed line is 99%
+confidence band.
+"""
+
+for var in df.columns:
+  plt.figure()
+  acPlot = pd.plotting.autocorrelation_plot(df[var])
+  plt.title(var)
+#### Joint Distribution Plots
+g = sns.PairGrid(df)
+# g.map(sns.scatterplot)
+# g.map(sns.histplot)
+# g.map_offdiag(sns.scatterplot)
+g.map_diag(sns.histplot, stat='density')
+g.map_diag(sns.kdeplot, color='.3')
+g.map_upper(sns.scatterplot)
+g.map_lower(sns.kdeplot)
+
+plt.show()
